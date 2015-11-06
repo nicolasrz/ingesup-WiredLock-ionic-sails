701f7470d4a97d97847c0e1f3f9c1c6d6c79bc87
nruiz-acpi <ruiz.nicolas64@gmail.com>
2015-11-06 11:58:26 +0100

    Door
    ajout generate api door

---
+++ Cloud/api/controllers/DoorController.js
@@ -0,0 +1,11 @@
+/**
+ * DoorController
+ *
+ * @description :: Server-side logic for managing doors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
