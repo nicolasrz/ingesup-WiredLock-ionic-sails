c967fef7c5b847b022d46af23574186a0a40c4ca
archloy <Archloy@users.noreply.github.com>
2015-11-08 14:12:07 +0100

    Apply KISS to controller : juste passing 'door' on update

--- Ihm/www/js/services/DoorsServ.js
+++ Ihm/www/js/services/DoorsServ.js
@@ -2,7 +2,7 @@ angular.module('doors.service', [])
 
 .factory('Doors', function($resource)
 {
-    return $resource('http://127.0.0.1:1337/door/:id', null,
+    return $resource('http://127.0.0.1:1337/door/:id', {id:'@id', state:'@state', owners:'@owners'},
             {
                 update: {
                     method: 'PUT'
